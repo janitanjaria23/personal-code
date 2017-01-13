0cdadf43d06bb4e06ac41fcc77dab3fcd100c163
Janit Anjaria <janit92@gmail.com>
2017-01-13 11:07:45 +0530

    changing svd plot code

--- svd_test.py
+++ svd_test.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 la = np.linalg
-words = ['I', 'like', 'deep', 'learning', 'enjoy', 'NLP', 'flying', '.']
+words = ['I', 'like', 'enjoy', 'deep', 'learning', 'NLP', 'flying', '.']
 
 x = np.array([[0, 2, 1, 0, 0, 0, 0, 0],
               [2, 0, 0, 1, 0, 1, 0, 0],
@@ -22,16 +22,13 @@ print vh
 
 eigvals = s**2 / np.cumsum(s)[-1]
 
-fig = plt.figure(figsize=(8,5))
-# sing_vals = np.arange(len(words)) + 1
-
-# plt.plot(u, s)
-# print a
+plt.xlim((-2, 2))
+plt.ylim((-2, 2))
 
 for i in range(0, len(words)):
+    print u[i, 0]
+    print u[i, 1]
     plt.text(u[i, 0], u[i, 1], words[i])
 
 print plt.text
-plt.savefig('testplot.png')
-
-# plt.show()
+plt.savefig('svd_plot.png')
