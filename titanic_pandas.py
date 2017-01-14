56d4a7d648dd59b65338bdfcf1b6dbdb05a974f6
Janit Anjaria <janit92@gmail.com>
2017-01-14 12:56:11 +0530

    data cleanup using pandas for creating training data

--- titanic_pandas.py
+++ titanic_pandas.py
@@ -3,16 +3,57 @@ import numpy as np
 import pylab as pyl
 
 df = pd.read_csv('/var/data/practice_data/titanic_train_data.csv', header=0)
-print df.head(3)
-print type(df)
-print df.dtypes
-print df.info()
-print df.describe()
-print df['Age'][0:10]
-print df['Cabin']
-
-print df[df['Age'] > 60][['Sex', 'Pclass', 'Age', 'Survived']]
-print df[df['Age'].isnull()][['Sex', 'Pclass', 'Age', 'Survived']]
+# print df.head(3)
+# print type(df)
+# print df.dtypes
+# print df.info()
+# print df.describe()
+# print df['Age'][0:10]
+# print df['Cabin']
+#
+# print df[df['Age'] > 60][['Sex', 'Pclass', 'Age', 'Survived']]
+# print df[df['Age'].isnull()][['Sex', 'Pclass', 'Age', 'Survived']]
 
 df['Age'].dropna().hist(bins=16, range=(0, 80), alpha=0.5)
-pyl.show()
+# pyl.show()
+
+
+df['Gender'] = 4
+
+df['Gender'] = df['Sex'].map(lambda x: x[0].upper())
+
+df['Gender'] = df['Sex'].map({'female': 0, 'male': 1}).astype(int)
+
+# print df.head(3)
+
+median_ages = np.zeros((2, 3))
+# print median_ages
+
+for i in range(0, 2):
+    for j in range(0, 3):
+        median_ages[i, j] = df[(df['Gender'] == i) & (df['Pclass'] == j + 1)]['Age'].dropna().median()
+
+print median_ages
+
+df['AgeFill'] = df['Age']
+
+print df.head(3)
+
+print df[df['Age'].isnull()][['Gender', 'Pclass', 'Age', 'AgeFill']].head(10)
+
+for i in range(0, 2):
+    for j in range(0, 3):
+        df.loc[(df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j + 1), 'AgeFill'] = median_ages[i, j]
+
+print df[df['Age'].isnull()][['Gender', 'Pclass', 'Age', 'AgeFill']].head(10)
+
+df['AgeIsNull'] = pd.isnull(df.Age).astype(int)
+
+df['FamilySize'] = df['SibSp'] + df['Parch']
+
+df['Age*Class'] = df['AgeFill'] * df['Pclass']
+
+df = df.dropna()
+
+train_data = df.values
+
