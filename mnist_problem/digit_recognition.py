319bdcd05679cf402d26d757443627651356941d
Janit Anjaria <janit92@gmail.com>
2017-04-13 00:39:51 +0530

    adding SGD classifier using sklearn

--- mnist_problem/digit_recognition.py
+++ mnist_problem/digit_recognition.py
@@ -1,6 +1,8 @@
 from sklearn.datasets.mldata import fetch_mldata
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import SGDClassifier
 
 
 def display_digit(some_digit):
@@ -17,5 +19,17 @@ def main():
     print 'Shape of y: ', y.shape
     display_digit(X[36000])
 
+    X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+    shuffle_index = np.random.permutation(60000)
+
+    X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+    y_train_5 = (y_train == 5)
+    y_test_5 = (y_test == 5)
+
+    sgd_clf = SGDClassifier(random_state=42)  # random_state helps to create reproducible results
+    sgd_clf.fit(X_train, y_train_5)
+    print sgd_clf.predict([X[36000]])
+    
 
 main()
